13153aa230c123ed2f285279c078455a651a3b58
Vitor Almeida Pires <vitoralmeidapires@gmail.com>
2021-12-05 14:57:06 -0300

    adicionando annotations no padrao de datas

--- src/main/java/com/api/faculdade/senac/piv/gestaoesteticaapi/entity/contasPagar/dto/ContasPagarDTO.java
+++ src/main/java/com/api/faculdade/senac/piv/gestaoesteticaapi/entity/contasPagar/dto/ContasPagarDTO.java
@@ -6,14 +6,19 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Data
 @NoArgsConstructor
 public class ContasPagarDTO {
 
+	@JsonFormat(pattern = "MM/dd/yyyy")
     private LocalDate emissao;
+	@JsonFormat(pattern = "MM/dd/yyyy")
     private LocalDate vencimento;
     private BigDecimal valor;
     private BigDecimal valorPago;
+    @JsonFormat(pattern = "MM/dd/yyyy")
     private LocalDate pagamento;
     private String status;
     private Long fornecedor;
